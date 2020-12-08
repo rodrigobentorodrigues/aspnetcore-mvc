d22bd5d6b6fd763cd32543d681a146b90adb5849
rodrigobentorodrigues <rodrigobentor2014@gmail.com>
2020-12-08 20:52:59 -0300

    Config to EntityFramework

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CourseMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,7 @@
+using CourseMVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +20,10 @@ namespace CourseMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure EntityFramework
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.
+                UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); 
             services.AddControllersWithViews();
         }
 
